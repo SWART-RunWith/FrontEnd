e70805e6920e9388587b8ae3f7db4726ac0ccd75
shin0112 <wnslcosltimo12@gmail.com>
2024-08-31 18:31:41 +0900

    feature : index 화면 구성

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Loading from '@/components/Loading';
+import Colors from '@/constants/Colors';
+
+const LoadingScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Loading />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.background,
+  },
+});
+
+export default LoadingScreen;
