82f1fad7a4667a59d2fb4b1b578953247c95f235
shin0112 <wnslcosltimo12@gmail.com>
2024-08-31 23:18:04 +0900

    feature : index 작성
    - 로딩 시간 2초, runwith 출력
    - 로딩 후, 온보딩 or 메인 화면

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import Loading from '@/components/Loading';
 import Colors from '@/constants/Colors';
 
-const LoadingScreen = () => {
+type RootStackParamList = {
+  onboarding: undefined;
+  home: undefined;
+};
+
+type LoadingScreenNavigationProp = StackNavigationProp<RootStackParamList, 'onboarding'>;
+
+const IndexScreen = () => {
+  const navigation = useNavigation<LoadingScreenNavigationProp>();
+
+  useEffect(() => {
+    const checkOnboardingStatus = async () => {
+      const hasSeenOnboarding = await AsyncStorage.getItem('hasSeenOnboarding');
+      if (hasSeenOnboarding) {
+        navigation.replace('home');
+      } else {
+        navigation.replace('onboarding');
+      }
+    };
+
+    const timer = setTimeout(() => {
+      checkOnboardingStatus();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
       <Loading />
@@ -20,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default LoadingScreen;
+export default IndexScreen;
