d505ab45901e92bd8ff24167e29824bc0c74544d
shin0112 <wnslcosltimo12@gmail.com>
2024-09-05 13:28:57 +0900

    feature : container style 재정의

--- constants/Styles.ts
+++ constants/Styles.ts
@@ -1,13 +1,16 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 import Colors from "@/constants/Colors";
+
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.background,
+    width: width,
+    height: height,
     alignItems: "center",
-    justifyContent: "center",
   },
 });
 
