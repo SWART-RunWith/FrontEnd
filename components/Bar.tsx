d726ade0606674ff4d7c7c3c0c6795521f6eb19c
shin0112 <wnslcosltimo12@gmail.com>
2024-09-08 23:59:04 +0900

    feature : 회원가입 바 수정
    - 이름, 이메일, 비밀번호, 전화번호, 생년월일, 키, 체중 제작

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -89,6 +89,133 @@ const DefaultBar = ({
   );
 };
 
+// 회원가입 Bar 컴포넌트
+const SignUpBar = ({
+  isRequired = true,
+  ...props
+}: BarProps) => {
+  return <DefaultBar isRequired={isRequired} {...props} />;
+};
+
+const SignUpNameBar = ({
+  label = "이름",
+  placeholder = "이름을 입력해주세요",
+  ...props
+}: BarProps) => {
+  return (
+    <SignUpBar
+      label={label}
+      placeholder={placeholder}
+      {...props}
+    />
+  );
+};
+
+const SignUpEmailBar = ({
+  label = "이메일",
+  placeholder = "이메일을 입력해주세요",
+  ...props
+}: BarProps) => {
+  return (
+    <SignUpBar
+      label={label}
+      placeholder={placeholder}
+      {...props}
+    />
+  );
+};
+
+const SignUpPasswordBar = ({
+  label = "비밀번호",
+  placeholder = "비밀번호를 입력해주세요",
+  ...props
+}: BarProps) => {
+  const [secureText, setSecureText] = useState(true);
+
+  return (
+    <SignUpBar
+      label={label}
+      placeholder={placeholder}
+      isSecure={secureText}
+      svgIcon={<Ionicons
+        name={secureText ? 'eye-off' : 'eye'}
+        size={20}
+        color="rgba(255, 255, 255, 0.6)"
+      />}
+      onButtonPressIn={() => setSecureText(false)}
+      onButtonPressOut={() => setSecureText(true)}
+      {...props}
+    />
+  );
+};
+
+const SignUpPhoneBar = ({
+  label = "전화번호",
+  placeholder = "전화번호를 입력해주세요",
+  ...props
+}: BarProps) => {
+  return (
+    <SignUpBar
+      label={label}
+      placeholder={placeholder}
+      {...props}
+    />
+  );
+};
+
+const SignUpBirthBar = ({
+  label = "생년월일",
+  placeholder = "생년월일을 입력해주세요",
+  ...props
+}: BarProps) => {
+  return (
+    <SignUpBar
+      label={label}
+      placeholder={placeholder}
+      {...props}
+    />
+  );
+};
+
+const SignUpHeightBar = ({
+  label = "키",
+  placeholder = "키를 입력해주세요",
+  ...props
+}: BarProps) => {
+  return (
+    <SignUpBar
+      label={label}
+      placeholder={placeholder}
+      {...props}
+    />
+  );
+};
+
+const SignUpWeightBar = ({
+  label = "체중",
+  placeholder = "체중을 입력해주세요",
+  ...props
+}: BarProps) => {
+  return (
+    <SignUpBar
+      label={label}
+      placeholder={placeholder}
+      {...props}
+    />
+  );
+};
+
+export {
+  SignUpBar,
+  SignUpNameBar,
+  SignUpEmailBar,
+  SignUpPasswordBar,
+  SignUpPhoneBar,
+  SignUpBirthBar,
+  SignUpHeightBar,
+  SignUpWeightBar,
+}
+
 // 로그인 Bar 컴포넌트
 const LoginBar = ({
   isRequired = false,
@@ -149,7 +276,6 @@ const styles = StyleSheet.create({
     fontSize: getSize(14),
     fontFamily: 'Pretendard-Medium',
     color: 'white',
-    marginLeft: getSize(Sizes.formMargin),
     marginRight: getSize(10),
   },
   inputContainer: {
