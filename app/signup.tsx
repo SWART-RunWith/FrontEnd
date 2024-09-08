54446ad8f0e7d7b9e463a6440eecf9babd8b2190
shin0112 <wnslcosltimo12@gmail.com>
2024-09-09 01:15:11 +0900

    feature : 입력값 화면에 출력

--- app/signup.tsx
+++ app/signup.tsx
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  Dimensions,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { useNavigation } from '@react-navigation/native';
@@ -12,12 +13,18 @@ import { useNavigation } from '@react-navigation/native';
 import Styles from '@/constants/Styles';
 import getSize from '@/scripts/getSize';
 import { LoginScreenNavigationProp } from '@/scripts/navigation';
-import { SignUpBirthBar, SignUpEmailBar, SignUpHeightBar, SignUpNameBar, SignUpPasswordBar, SignUpPhoneBar, SignUpWeightBar } from '@/components/Bar';
+import {
+  SignUpBirthBar,
+  SignUpEmailBar,
+  SignUpHeightBar,
+  SignUpNameBar,
+  SignUpPasswordBar,
+  SignUpPhoneBar,
+  SignUpWeightBar
+} from '@/components/bar/SignUpBar';
 import { SignUpHeader } from '@/components/Header';
 import { DefaultButton, SignUpButton } from '@/components/Button';
 import Colors from '@/constants/Colors';
-
-const { width, height } = Dimensions.get('window');
 
 const SignUpScreen: React.FC = () => {
   const navigation = useNavigation<LoginScreenNavigationProp>();
@@ -27,57 +34,86 @@ const SignUpScreen: React.FC = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
 
   return (
     <View style={Styles.container}>
-      <KeyboardAwareScrollView
-        contentContainerStyle={Styles.keyboardScroll}
-        extraScrollHeight={getSize(20)}
-      >
-        <SignUpHeader
-          onPress={() => {
-            step === 1
-              ? navigation.goBack()
-              : setStep(prevStep => prevStep - 1)
-          }} />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <KeyboardAwareScrollView
+          contentContainerStyle={Styles.keyboardScroll}
+          extraScrollHeight={getSize(20)}
+        >
+          <SignUpHeader
+            onPress={() => {
+              step === 1
+                ? navigation.goBack()
+                : setStep(prevStep => prevStep - 1)
+            }} />
 
-        {/* 회원가입 1 */}
-        {step === 1 && (
-          <View style={styles.barContainer}>
-            <SignUpNameBar />
-            <SignUpEmailBar />
-            <SignUpPasswordBar />
-            <SignUpPhoneBar />
-            <View style={styles.ButtonContainer}>
-              <DefaultButton
-                text="다음으로"
-                onPress={() => {
-                  setStep(prevStep => prevStep + 1);
-                }}
+          {/* 회원가입 1 */}
+          {step === 1 && (
+            <View style={styles.barContainer}>
+              <SignUpNameBar
+                value={name}
+                onChangeText={setName}
               />
-            </View>
-          </View>)}
+              <SignUpEmailBar
+                value={email}
+                onChangeText={setEmail}
+              />
+              <SignUpPasswordBar
+                value={password}
+                onChangeText={setPassword}
+              />
+              <SignUpPhoneBar
+                value={phone}
+                onChangeText={setPhone}
+              />
+              <View style={styles.ButtonContainer}>
+                <DefaultButton
+                  text="다음으로"
+                  onPress={() => {
+                    setStep(prevStep => prevStep + 1);
+                  }}
+                />
+              </View>
+            </View>)}
 
-        {/* 회원가입 2 */}
-        {step === 2 && (
-          <View style={styles.barContainer}>
-            <SignUpNameBar />
-            <SignUpBirthBar />
-            <SignUpHeightBar isRequired={false} />
-            <SignUpWeightBar isRequired={false} />
-            <View style={styles.ButtonContainer}>
-              {/* to do : 회원가입 api 연동 */}
-              <SignUpButton onPress={() => { navigation.replace('home') }} />
-            </View>
-          </View>)}
+          {/* 회원가입 2 */}
+          {step === 2 && (
+            <View style={styles.barContainer}>
+              <SignUpNameBar />
+              <SignUpBirthBar
+                value={dateOfBirth}
+                onChangeText={setDateOfBirth}
+              />
+              <SignUpHeightBar
+                isRequired={false}
+                value={height}
+                onChangeText={setHeight}
+              />
+              <SignUpWeightBar
+                isRequired={false}
+                value={weight}
+                onChangeText={setWeight}
+              />
+              <View style={styles.ButtonContainer}>
+                {/* to do : 회원가입 api 연동 */}
+                <SignUpButton onPress={() => { navigation.replace('home') }} />
+              </View>
+            </View>)}
 
-        <View style={styles.textContainer}>
-          <Text style={styles.loginText}>계정이 있으신가요? </Text>
-          <TouchableOpacity onPress={() => navigation.replace('login')}>
-            <Text style={[styles.loginText, styles.highlight]}>로그인</Text>
-          </TouchableOpacity>
-        </View>
-      </KeyboardAwareScrollView>
+          <View style={styles.textContainer}>
+            <Text style={styles.loginText}>계정이 있으신가요? </Text>
+            <TouchableOpacity onPress={() => navigation.replace('login')}>
+              <Text style={[styles.loginText, styles.highlight]}>로그인</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAwareScrollView>
+      </TouchableWithoutFeedback>
     </View >
   );
 };
