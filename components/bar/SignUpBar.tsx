0e959b74f68d1388025d494597c257d8273ca021
shin0112 <wnslcosltimo12@gmail.com>
2024-09-09 15:34:50 +0900

    design : 성별 바 container 높이 수정

--- components/bar/SignUpBar.tsx
+++ components/bar/SignUpBar.tsx
@@ -248,7 +248,6 @@ const SignUpGenderBar: React.FC<SignUpGenderBarProps> = ({
 const styles = StyleSheet.create({
   container: {
     marginHorizontal: getSize(Sizes.formMargin),
-    height: getSize(81),
   },
   labelContainer: {
     flexDirection: 'row',
