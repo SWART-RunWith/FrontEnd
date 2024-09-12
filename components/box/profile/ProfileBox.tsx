58b6895d3ce4361f1630af3c93f50964ead26ea4
shin0112 <wnslcosltimo12@gmail.com>
2024-09-12 14:30:29 +0900

    feature : 프로필 박스 컴포넌트 작성

---
+++ components/box/profile/ProfileBox.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions
+} from 'react-native';
+
+import getSize from '@/scripts/getSize';
+import Colors from '@/constants/Colors';
+
+const { width } = Dimensions.get('window');
+
+interface MyBestBoxProps {
+  title: string;
+  value: string;
+  description: string;
+  additionalInfo: string;
+}
+
+const MyBestBox: React.FC<MyBestBoxProps> = ({
+  title,
+  value,
+  description,
+  additionalInfo
+}) => {
+  return (
+    <View style={styles.boxContainer}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.value}>{value}</Text>
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.description}>{description}</Text>
+        <Text style={styles.additionalInfo}>{additionalInfo}</Text>
+      </View>
+    </View>
+  );
+};
+
+export {
+  MyBestBox
+};
+
+const ShoesImage = require('@/assets/images/shoes.png'); // 이미지 경로를 실제 경로로 변경하세요
+
+interface RunningShoesBoxProps {
+  brand: string;
+  model: string;
+  edition: string;
+}
+
+const RunningShoesBox: React.FC<RunningShoesBoxProps> = ({
+  brand,
+  model,
+  edition
+}) => {
+  return (
+    <View style={styles.boxContainer}>
+      <Image
+        source={ShoesImage}
+        style={styles.shoesImage}
+        resizeMode="contain"
+      />
+
+      <View style={styles.textContainer}>
+        <Text style={styles.brand}>{brand}</Text>
+        <Text style={styles.model}>{model}</Text>
+        <Text style={styles.edition}>{edition}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  boxContainer: {
+    backgroundColor: Colors.grayBox,
+    borderRadius: 10,
+    padding: getSize(20),
+  },
+  title: {
+    fontSize: getSize(20),
+    color: 'white',
+    fontFamily: 'Pretendard-SemiBold',
+  },
+  descriptionContainer: {
+    marginTop: getSize(18),
+  },
+  value: {
+    fontSize: getSize(32),
+    color: Colors.main,
+    fontFamily: 'Pretendard-ExtraBold',
+    marginTop: getSize(8),
+  },
+  description: {
+    fontSize: getSize(16),
+    color: 'white',
+    fontFamily: 'Pretendard-Medium',
+  },
+  additionalInfo: {
+    fontSize: getSize(14),
+    color: 'white',
+    fontFamily: 'Pretendard-Light',
+    marginTop: getSize(4),
+  },
+  shoesImage: {
+    width: getSize(220),
+    height: getSize(130),
+  },
+  textContainer: {
+    width: '100%',
+  },
+  brand: {
+    fontSize: getSize(20),
+    color: Colors.main,
+    fontFamily: 'Pretendard-Medium',
+  },
+  model: {
+    fontSize: getSize(14),
+    color: 'white',
+    fontFamily: 'Pretendard-Light',
+    marginTop: getSize(4),
+  },
+  edition: {
+    fontSize: getSize(14),
+    color: 'white',
+    fontFamily: 'Pretendard-Light',
+    marginTop: getSize(10),
+  },
+});
+
+export {
+  RunningShoesBox
+};
