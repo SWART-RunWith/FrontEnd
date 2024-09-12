1a219d46e86c1c1af13b70ed1c6e706f168b4dcd
shin0112 <wnslcosltimo12@gmail.com>
2024-09-12 21:12:24 +0900

    refactor : 헤더 재구성

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,167 +1,121 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextStyle, ViewStyle } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Svg, Path } from 'react-native-svg';
 
 import Colors from '@/constants/Colors';
 import Sizes from '@/constants/Sizes';
 import getSize from '@/scripts/getSize';
-import { Path, Svg } from 'react-native-svg';
+import BackSvgIcon from '@/assets/icons/back.svg';
+import EditSvgIcon from '@/assets/icons/edit.svg';
+import SettingSvgIcon from '@/assets/icons/setting.svg';
 
 const { width } = Dimensions.get('window');
 
-// 아이콘 객체의 타입 정의
-interface IconObject {
-  svg?: React.ReactNode;
-  onPress: () => void;
+// 공통 타입 정의
+interface IconProps {
+  onPress?: () => void;
 }
 
-// DefaultHeader: 아이콘을 렌더링 가능하게 수정
-interface DefaultHeaderProps {
-  onPress?: () => void;
+// BackProps 정의
+const BackIcon: React.FC<IconProps> = ({ onPress }) => {
+  const navigation = useNavigation();
+  return (
+    <TouchableOpacity
+      onPress={onPress || (() => navigation.goBack())}
+      style={styles.iconButton}
+    >
+      <BackSvgIcon width={getSize(14)} height={getSize(24)} />
+    </TouchableOpacity>
+  );
+};
+
+// EditProps 정의
+const EditIcon: React.FC<IconProps> = ({ onPress }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={styles.iconButton}
+  >
+    <EditSvgIcon width={getSize(24)} height={getSize(24)} />
+  </TouchableOpacity>
+);
+
+// SettingProps 정의
+const SettingIcon: React.FC<IconProps> = ({ onPress }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={styles.iconButton}
+  >
+    <SettingSvgIcon width={getSize(24)} height={getSize(23)} />
+  </TouchableOpacity>
+);
+
+// TextProps 정의
+interface TextProps {
   text?: string;
   fontSize?: number;
   fontFamily?: string;
   fontColor?: string;
-  leftSvg?: React.ReactNode;
-  rightSvg?: React.ReactNode;
-  rightSvg2?: React.ReactNode;
-  titleStyle?: TextStyle;
-  containerStyle?: ViewStyle;
 }
 
-const DefaultHeader: React.FC<DefaultHeaderProps> = ({
+const HeaderText: React.FC<TextProps> = ({
   text = 'RUNWITH',
   fontSize = 20,
   fontFamily = 'Hanson',
   fontColor = Colors.main,
-  leftSvg,
-  rightSvg,
-  rightSvg2,
-  containerStyle = {},
-}) => {
-  return (
-    <View style={[styles.container, containerStyle]}>
-      {/* 왼쪽 SVG 아이콘 */}
-      <View style={styles.leftIcons}>
-        {leftSvg && (
-          <TouchableOpacity style={styles.iconButton}>
-            {leftSvg}
-          </TouchableOpacity>
-        )}
-      </View>
+}) => (
+  <Text style={{
+    fontSize: getSize(fontSize),
+    fontFamily,
+    color: fontColor
+  }}>
+    {text}
+  </Text>
+);
 
-      {/* 중앙 텍스트 */}
-      <View style={styles.centerText}>
-        <Text style={{
-          fontSize: getSize(fontSize),
-          fontFamily,
-          color: fontColor,
-        }}>
-          {text}
-        </Text>
-      </View>
-
-      {/* 오른쪽 SVG 아이콘 1 */}
-      <View style={styles.rightIcons}>
-        {rightSvg && (
-          <TouchableOpacity style={styles.iconButton}>
-            {rightSvg}
-          </TouchableOpacity>
-        )}
-      </View>
-
-      {/* 오른쪽 SVG 아이콘 2 */}
-      <View style={styles.rightIcons}>
-        {rightSvg2 && (
-          <TouchableOpacity style={styles.iconButton}>
-            {rightSvg2}
-          </TouchableOpacity>
-        )}
-      </View>
-    </View>
-  );
-};
-
-// BackHeader: DefaultHeader를 확장하여 기본적으로 뒤로가기 아이콘 추가
-interface BackHeaderProps extends DefaultHeaderProps {
-  leftIcons?: IconObject[];
+interface CombinedHeaderProps {
+  backProps?: IconProps;
+  editProps?: IconProps;
+  settingProps?: IconProps;
+  textProps?: TextProps;
+  containerStyle?: ViewStyle;
 }
 
-const BackHeader: React.FC<BackHeaderProps> = ({
-  text = '',
-  fontSize = 20,
-  fontFamily = 'Hanson',
-  fontColor = Colors.main,
-  onPress,
+const CombinedHeader: React.FC<CombinedHeaderProps> = ({
+  backProps,
+  editProps,
+  settingProps,
+  textProps,
   containerStyle = {},
-  leftSvg = (
-    <Svg width={getSize(14)} height={getSize(24)} viewBox="0 0 14 24" fill="none">
-      <Path
-        d="M13.3708 0.470024C12.7392 -0.156675 11.7208 -0.156675 11.0892 0.470024L0.377051 11.0983C-0.125684 11.5971 -0.125684 12.4029 0.377051 12.9017L11.0892 23.53C11.7208 24.1567 12.7392 24.1567 13.3708 23.53C14.0024 22.9033 14.0024 21.8929 13.3708 21.2662L4.03799 11.9936L13.3837 2.72102C14.0024 2.10711 14.0024 1.08393 13.3708 0.470024Z"
-        fill="white"
-      />
-    </Svg>
-  ),
-}) => {
-  const navigation = useNavigation();
+}) => (
+  <View style={[styles.container, containerStyle]}>
+    {/* Back Icon */}
+    {backProps && (
+      <View style={styles.leftIcons}>
+        <BackIcon {...backProps} />
+      </View>
+    )}
 
-  return (
-    <DefaultHeader
-      text={text}
-      fontSize={fontSize}
-      fontFamily={fontFamily}
-      fontColor={fontColor}
-      leftSvg={(
-        <TouchableOpacity
-          onPress={onPress}
-          style={styles.iconButton}
-        >
-          {leftSvg}
-        </TouchableOpacity>
-      )}
-      containerStyle={containerStyle}
-    />
-  );
-};
+    {/* Text */}
+    <View style={styles.centerText}>
+      <HeaderText {...textProps} />
+    </View>
 
-const KoBackHeader: React.FC<BackHeaderProps> = ({
-  fontFamily = 'Pretendard-SemiBold',
-  fontSize = Sizes.pageTitle,
-  ...props
-}) => {
-  return <BackHeader {...props} fontFamily={fontFamily} fontSize={fontSize} />;
-};
+    {/* Edit Icon */}
+    {editProps && (
+      <View style={styles.rightIcons}>
+        <EditIcon {...editProps} />
+      </View>
+    )}
 
-// LoginHeader와 SignUpHeader는 BackHeader를 사용
-const LoginHeader: React.FC<BackHeaderProps> = ({
-  text = '로그인',
-  ...props
-}) => {
-  return <KoBackHeader {...props} text={text} />;
-};
-
-const SignUpHeader: React.FC<BackHeaderProps> = ({
-  text = '회원가입',
-  ...props
-}) => {
-  return <KoBackHeader {...props} text={text} />;
-
-};
-
-const TermsAgreeHeader: React.FC<BackHeaderProps> = ({
-  text = '약관 동의',
-  ...props
-}) => {
-  return <KoBackHeader {...props} text={text} />;
-
-};
-
-const ProfileHeader: React.FC<BackHeaderProps> = ({
-  text = '프로필 설정',
-  ...props
-}) => {
-  return <KoBackHeader {...props} text={text} />;
-};
+    {/* Settings Icon */}
+    {settingProps && (
+      <View style={styles.rightIcons}>
+        <SettingIcon {...settingProps} />
+      </View>
+    )}
+  </View>
+);
 
 // 스타일 정의
 const styles = StyleSheet.create({
@@ -188,14 +142,15 @@ const styles = StyleSheet.create({
     marginHorizontal: getSize(10),
   },
   centerText: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
 export {
-  DefaultHeader,
-  BackHeader,
-  LoginHeader,
-  SignUpHeader,
-  TermsAgreeHeader,
-  ProfileHeader,
+  CombinedHeader,
+  BackIcon,
+  EditIcon,
+  SettingIcon,
+  HeaderText
 };
