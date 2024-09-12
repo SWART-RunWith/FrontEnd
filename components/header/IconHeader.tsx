9a17d19269fe8feeb072fcfcc4d601bf89b8115c
shin0112 <wnslcosltimo12@gmail.com>
2024-09-12 22:14:57 +0900

    refactor : icon / text only header로 세분화

---
+++ components/header/IconHeader.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import {
+  CombinedHeader,
+  CombinedHeaderProps,
+} from '@/components/header/Header';
+
+const { width } = Dimensions.get('window');
+
+interface BackIconProps {
+  onPressBack: () => void;
+}
+
+const SignUpHeader: React.FC<BackIconProps> = ({
+  onPressBack
+}) => {
+  return (
+    <CombinedHeader
+      backProps={{ onPress: onPressBack }}
+      textProps={{
+        text: '회원가입',
+      }}
+    />
+  );
+};
+
+const TermsAgreeHeader: React.FC<BackIconProps> = ({
+  onPressBack
+}) => {
+  return (
+    <CombinedHeader
+      backProps={{ onPress: onPressBack }}
+      textProps={{
+        text: '약관 동의',
+      }}
+    />
+  );
+};
+
+const MyFolderHeader: React.FC<BackIconProps> = ({
+  onPressBack
+}) => {
+  return (
+    <CombinedHeader
+      backProps={{ onPress: onPressBack }}
+      textProps={{
+        text: '내 폴더',
+        fontColor: 'white',
+      }}
+      settingProps={{ onPress: () => console.log('설정 버튼 클릭') }}
+    />
+  );
+};
+
+const ProfileSettingHeader: React.FC<BackIconProps> = ({
+  onPressBack
+}) => {
+  return (
+    <CombinedHeader
+      backProps={{ onPress: onPressBack }}
+      textProps={{
+        text: '프로필 설정',
+      }}
+      settingProps={{ onPress: () => console.log('설정 버튼 클릭') }} // 설정 아이콘 추가
+    />
+  );
+};
+
+const ProfileHeader: React.FC<CombinedHeaderProps> = ({
+  backProps,
+  showBackIcon,
+}) => {
+  return (
+    <CombinedHeader
+      showBackIcon={showBackIcon}
+      backProps={{
+        onPress: backProps?.onPress
+      }}
+      textProps={{
+        text: '',
+      }}
+      settingProps={{ onPress: () => console.log('설정 버튼 클릭') }} // 설정 아이콘 추가
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: width,
+  },
+});
+
+export {
+  SignUpHeader,
+  TermsAgreeHeader,
+  MyFolderHeader,
+  ProfileSettingHeader,
+  ProfileHeader,
+};
