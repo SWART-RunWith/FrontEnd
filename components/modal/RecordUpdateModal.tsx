dc1a9465f5e16da05fb626e42b300ce26baae611
shin0112 <wnslcosltimo12@gmail.com>
2024-09-13 18:06:44 +0900

    feature : 기록 수정 모달 picker 연결

--- components/modal/RecordUpdateModal.tsx
+++ components/modal/RecordUpdateModal.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
   Dimensions,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
 import RNModal from 'react-native-modal';
 
@@ -12,15 +14,18 @@ import {
   ModalHeader,
   ModalRecordInput,
 } from '@/components/modal/UpdateModal';
+import { TimerModal } from '@/components/modal/TimerModal';
 import Colors from '@/constants/Colors';
+import Sizes from '@/constants/Sizes';
 import getSize from '@/scripts/getSize';
+import CancelIcon from '@/assets/icons/cancel.svg';
 
 const { width } = Dimensions.get('window');
 
 interface ModalProps {
   label: string;
-  placeholder: string;
   title: string;
+  type: 'distance' | 'pace' | 'time';
 }
 
 interface UpdateValueProps {
@@ -50,63 +55,99 @@ const RecordUpdateModal: React.FC<ModalProps & UpdateValueProps> = ({
   onCancel,
   onConfirm,
   label = '',
-  placeholder = '값을 입력해주세요',
   title = '수정',
-}) => (
-  <RNModal
-    isVisible={isVisible}
-    onBackdropPress={onCancel}
-    style={styles.bottomModal}
-  >
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.modalContainer}>
-        <View style={styles.modalContent}>
-          <ModalHeader
-            title={title}
-            onCancel={onCancel}
-            onConfirm={() => {
-              if (onConfirm) {
-                onConfirm(
-                  value === '' ? '' : value,
-                  shoesValue === '' ? '' : shoesValue,
-                  memoValue === '' ? '' : memoValue
-                );
-              }
-            }}
-          />
+  type = 'distance',
+}) => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedValue, setSelectedValue] = useState('');
 
-          <View style={styles.modalInputContainer}>
-            <ModalRecordInput
-              label={label}
-              value={value}
-              onChangeText={onChangeText}
-              placeholder={placeholder}
+  const showModal = () => {
+    setModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setModalVisible(false);
+  };
+
+  const handleConfirm = (value: string) => {
+    setSelectedValue(value);
+    setModalVisible(false);
+  };
+
+  return (
+    <RNModal
+      isVisible={isVisible}
+      onBackdropPress={onCancel}
+      style={styles.bottomModal}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <ModalHeader
+              title={title}
+              onCancel={onCancel}
+              onConfirm={() => {
+                if (onConfirm) {
+                  onConfirm(
+                    value === '' ? '0' : value,
+                    shoesValue === '' ? '' : shoesValue,
+                    memoValue === '' ? '' : memoValue
+                  );
+                }
+              }}
             />
-            <ModalRecordInput
-              label='신발 정보'
-              value={shoesValue}
-              onChangeText={onChangeShoes}
-              placeholder='신발 정보를 입력해주세요'
-            />
-            <ModalRecordInput
-              label='메모'
-              value={memoValue}
-              onChangeText={onChangeMemo}
-              placeholder='메모를 입력해주세요'
+
+            <View style={styles.modalInputContainers}>
+              <TouchableWithoutFeedback
+                onPress={() => showModal()}>
+                <View style={styles.modalInputContainer}>
+                  <View style={styles.inputHeader}>
+                    <Text style={styles.modalInputTitle}>{label}</Text>
+                    <TouchableOpacity
+                      style={{
+                        marginTop: getSize(11),
+                        marginRight: getSize(10),
+                      }}
+                      onPress={() => onChangeText('')}>
+                      <CancelIcon width={getSize(20)} height={getSize(20)} fill={Colors.gray} />
+                    </TouchableOpacity>
+                  </View>
+                  <Text style={styles.modalInput}>{value}</Text>
+                </View>
+              </TouchableWithoutFeedback>
+              <ModalRecordInput
+                label='신발 정보'
+                value={shoesValue}
+                onChangeText={onChangeShoes}
+                placeholder='신발 정보를 입력해주세요'
+              />
+              <ModalRecordInput
+                label='메모'
+                value={memoValue}
+                onChangeText={onChangeMemo}
+                placeholder='메모를 입력해주세요'
+              />
+            </View>
+
+            <TimerModal
+              isVisible={isModalVisible}
+              type={type}
+              onCancel={handleCancel}
+              onConfirm={handleConfirm}
             />
           </View>
         </View>
-      </View>
-    </TouchableWithoutFeedback>
-  </RNModal>
-);
+      </TouchableWithoutFeedback>
+    </RNModal>
+  );
+};
 
 const DistanceUpdateModal: React.FC<UpdateValueProps> = (props) => (
   <RecordUpdateModal
     {...props}
     title="최장 거리 수정"
     label="KM"
-    placeholder="최장 거리를 입력해주세요"
+    type='distance'
   />
 );
 
@@ -115,7 +156,7 @@ const TimeUpdateModal: React.FC<UpdateValueProps> = (props) => (
     {...props}
     title="최장 시간 수정"
     label="TIME"
-    placeholder="최장 시간을 입력해주세요"
+    type='time'
   />
 );
 
@@ -124,7 +165,7 @@ const PaceUpdateModal: React.FC<UpdateValueProps> = (props) => (
     {...props}
     title="최고 페이스 수정"
     label="TIME"
-    placeholder="최고 페이스를 입력해주세요"
+    type='pace'
   />
 );
 
@@ -145,10 +186,36 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     alignItems: 'center',
   },
-  modalInputContainer: {
+  modalInputContainers: {
     marginTop: getSize(48),
     gap: getSize(12),
-  }
+  },
+  inputHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalInputContainer: {
+    paddingLeft: getSize(18),
+    width: width - getSize(Sizes.formMargin) * 2,
+    height: getSize(99),
+    borderColor: 'white',
+    borderWidth: getSize(1),
+    borderRadius: 20,
+  },
+  modalInputTitle: {
+    fontSize: getSize(20),
+    color: Colors.main,
+    fontFamily: 'Pretendard-SemiBold',
+    marginTop: getSize(13),
+  },
+  modalInput: {
+    marginRight: getSize(55),
+    marginBottom: getSize(25),
+    fontSize: getSize(16),
+    color: 'white',
+    fontFamily: 'Pretendard-Medium',
+    marginTop: getSize(10),
+  },
 });
 
 export {
