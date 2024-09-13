47912123e4d24aa832505b8bf66c3af9abf8ac49
shin0112 <wnslcosltimo12@gmail.com>
2024-09-14 01:25:37 +0900

    design : 설정 화면 디자인 변경
    - 텍스트 크기, 폰트 변경
    - bar, box 조절

--- app/user/setting.tsx
+++ app/user/setting.tsx
@@ -5,12 +5,18 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
+  Dimensions,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { HomeScreenNavigationProp } from '@/scripts/navigation';
 import Colors from '@/constants/Colors';
 import getSize from '@/scripts/getSize';
+import { SettingHeader } from '@/components/header/IconHeader';
+import Sizes from '@/constants/Sizes';
+import RightArrowIcon from '@/assets/icons/rightArrow.svg';
+
+const { width } = Dimensions.get('window');
 
 const SettingScreen = () => {
   const navigation = useNavigation<HomeScreenNavigationProp>();
@@ -21,130 +27,155 @@ const SettingScreen = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.header}>설정</Text>
+      <SettingHeader />
 
-      <View style={styles.settingSection}>
+      <View style={styles.textContainer}>
         <Text style={styles.label}>이메일</Text>
         <Text style={styles.value}>tama0412@naver.com</Text>
       </View>
 
-      <View style={styles.settingSection}>
+      <View style={styles.bar} />
+
+      <View style={styles.textContainer}>
         <Text style={styles.label}>전화 번호</Text>
         <Text style={styles.value}>+82 01 1234 5678</Text>
       </View>
 
+      <View style={styles.bar} />
+
       <TouchableOpacity
-        style={styles.settingSection}
+        style={styles.textContainer}
         onPress={() => navigation.navigate('MyInfo')}>
         <Text style={styles.label}>내 정보</Text>
-        <Text style={styles.arrow}>›</Text>
+        <RightArrowIcon width={getSize(8)} height={getSize(14)} />
       </TouchableOpacity>
 
+      <View style={styles.bar} />
+
       <TouchableOpacity
-        style={styles.settingSection}
+        style={styles.textContainer}
         onPress={() => navigation.navigate('MeasurementUnit')}>
         <Text style={styles.label}>측정 단위</Text>
-        <Text style={styles.arrow}>›</Text>
+        <RightArrowIcon width={getSize(8)} height={getSize(14)} />
       </TouchableOpacity>
 
+      <View style={styles.box} />
+
       <TouchableOpacity
-        style={styles.settingSection}
+        style={styles.textContainer}
         onPress={() => navigation.navigate('Notifications')}>
         <Text style={styles.label}>알림</Text>
-        <Text style={styles.arrow}>›</Text>
+        <RightArrowIcon width={getSize(8)} height={getSize(14)} />
       </TouchableOpacity>
+
+      <View style={styles.bar} />
 
       <TouchableOpacity
-        style={styles.settingSection}
+        style={styles.textContainer}
         onPress={() => navigation.navigate('ProfileVisibility')}>
         <Text style={styles.label}>프로필 공개 범위</Text>
-        <Text style={styles.arrow}>›</Text>
+        <RightArrowIcon width={getSize(8)} height={getSize(14)} />
       </TouchableOpacity>
 
-      <View style={styles.settingSection}>
+      <View style={styles.box} />
+
+      <View style={styles.textContainer}>
         <Text style={styles.label}>국가/지역</Text>
         <Text style={styles.value}>대한민국</Text>
       </View>
 
-      <View style={styles.settingSection}>
+      <View style={styles.bar} />
+
+      <View style={styles.textContainer}>
         <Text style={styles.label}>버전 정보</Text>
         <Text style={styles.value}>1.0 V</Text>
       </View>
 
+      <View style={styles.bar} />
+
       <TouchableOpacity
-        style={styles.settingSection}
+        style={styles.textContainer}
         onPress={() => navigation.navigate('PrivacyPolicy')}>
         <Text style={styles.label}>개인정보 처리 방침</Text>
-        <Text style={styles.arrow}>›</Text>
+        <RightArrowIcon width={getSize(8)} height={getSize(14)} />
       </TouchableOpacity>
 
+      <View style={styles.bar} />
+
       <TouchableOpacity
-        style={styles.settingSection}
+        style={styles.textContainer}
         onPress={() => navigation.navigate('ContactUs')}>
         <Text style={styles.label}>문의하기</Text>
-        <Text style={styles.arrow}>›</Text>
+        <RightArrowIcon width={getSize(8)} height={getSize(14)} />
       </TouchableOpacity>
 
+      <View style={styles.box} />
+
       <TouchableOpacity
-        style={styles.settingSection}
+        style={styles.textContainer}
         onPress={() => navigation.navigate('DeleteAccount')}>
         <Text style={styles.label}>계정 탈퇴</Text>
-        <Text style={styles.arrow}>›</Text>
+        <RightArrowIcon width={getSize(8)} height={getSize(14)} />
       </TouchableOpacity>
 
+      <View style={styles.box} />
+
       <TouchableOpacity
-        style={styles.logoutSection}
+        style={styles.textContainer}
         onPress={handleLogout}>
         <Text style={styles.logoutText}>로그아웃</Text>
       </TouchableOpacity>
+
+      <View style={styles.bottomBox} />
+
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: getSize(20),
-    backgroundColor: '#400000', // Adjust the background color based on your image
-    flexGrow: 1,
-  },
-  header: {
-    fontSize: getSize(24),
-    fontWeight: 'bold',
-    color: '#FFD700', // Yellow color for header
-    marginBottom: getSize(20),
-    textAlign: 'center',
-  },
-  settingSection: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    backgroundColor: Colors.background,
+    width: width,
+    height: getSize(1058),
     alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgba(255, 255, 255, 0.3)', // Slightly transparent white for borders
-    paddingVertical: getSize(15),
+  },
+  bar: {
+    backgroundColor: Colors.gray,
+    width: width,
+    height: getSize(0.5),
+  },
+  box: {
+    backgroundColor: Colors.lightGrayBox,
+    height: getSize(54),
+    width: width,
+  },
+  textContainer: {
+    width: width,
+    height: getSize(54),
+    paddingHorizontal: getSize(Sizes.formMargin),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   label: {
-    fontSize: getSize(16),
-    color: '#FFFFFF',
+    fontSize: getSize(14),
+    fontFamily: 'Pretendard-Light',
+    color: 'white',
   },
   value: {
-    fontSize: getSize(16),
-    color: '#FFFFFF',
-    opacity: 0.8,
-  },
-  arrow: {
-    fontSize: getSize(18),
-    color: '#FFFFFF',
-  },
-  logoutSection: {
-    marginTop: getSize(20),
-    borderTopWidth: 1,
-    borderTopColor: 'rgba(255, 255, 255, 0.3)',
-    paddingTop: getSize(15),
+    fontSize: getSize(14),
+    fontFamily: 'Pretendard-Light',
+    color: 'white',
   },
   logoutText: {
     fontSize: getSize(18),
-    color: '#FF4500', // Red color for the logout button
+    color: '#FF0000',
     textAlign: 'center',
+  },
+  bottomBox: {
+    backgroundColor: Colors.lightGrayBox,
+    height: getSize(150),
+    width: width,
   },
 });
 
