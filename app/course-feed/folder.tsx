86672a8f2e7731d9327723f68c8efac1ffd1455c
shin0112 <wnslcosltimo12@gmail.com>
2024-09-16 23:31:36 +0900

    feature : 임시 화면 작성

--- app/course-feed/folder.tsx
+++ app/course-feed/folder.tsx
@@ -1,9 +1,34 @@
 import { Text, View } from "react-native";
 
+import { DefaultButton } from "@/components/button/Button";
+import Styles from '@/constants/Styles';
+import getSize from "@/scripts/getSize";
+import { useNavigation } from "@react-navigation/native";
+import { HomeScreenNavigationProp } from "@/scripts/navigation";
+
 const FolderScreen = () => {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+
   return (
-    <View>
-      <Text>This is Course Save Folder Screen.</Text>
+    <View style={Styles.container}>
+      <View style={{
+        justifyContent: 'center',
+        height: '30%',
+      }}>
+        <Text style={{
+          color: 'white',
+          fontSize: getSize(20),
+        }}>
+          This is Course Save Folder Screen.
+        </Text>
+      </View>
+
+      <DefaultButton
+        style={{
+          marginTop: getSize(100),
+        }}
+        onPress={() => navigation.navigate('home')}
+      />
     </View>
   );
 };
