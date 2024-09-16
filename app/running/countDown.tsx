420131d50e82ce4a65996c582102a3ca5ede0b70
shin0112 <wnslcosltimo12@gmail.com>
2024-09-16 14:09:48 +0900

    feature : navi 연결(카운트 다운 화면 -> 러닝 중 화면)

--- app/running/countDown.tsx
+++ app/running/countDown.tsx
@@ -2,19 +2,26 @@ import Colors from '@/constants/Colors';
 import getSize from '@/scripts/getSize';
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RunningScreenNavigationProp } from '@/scripts/navigation';
 
 const { width, height } = Dimensions.get('window');
 
 const CountDown: React.FC = () => {
+  const navigation = useNavigation<RunningScreenNavigationProp>();
+
   const [count, setCount] = useState(3);
 
   useEffect(() => {
-    // 타이머 설정
     if (count > 0) {
       const timer = setInterval(() => {
         setCount(prevCount => prevCount - 1);
       }, 1000);
       return () => clearInterval(timer);
+    } else {
+      setTimeout(() => {
+        navigation.replace('running/running');
+      }, 1000);
     }
   }, [count]);
 
@@ -29,7 +36,6 @@ const CountDown: React.FC = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Colors.main,
