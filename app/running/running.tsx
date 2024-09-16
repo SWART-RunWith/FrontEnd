1a395b637e53c33fe8b651ac8a5be30d91bb66d5
shin0112 <wnslcosltimo12@gmail.com>
2024-09-16 14:44:10 +0900

    design : 디자인 변경
    - 배경 색 변경
    - 러닝 정보 관련 폰트 및 위치 조정
    - 버튼 크기 및 위치 조정
    - 하트 아이콘 추가

--- app/running/running.tsx
+++ app/running/running.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Ionicons';
 
-import { RunningScreenNavigationProp, RunningStackParamList } from '@/scripts/navigation';
-import { EndButton, PauseButton, PlayButton } from '@/components/button/RunningButton';
+import {
+  EndButton,
+  PauseButton,
+  PlayButton
+} from '@/components/button/RunningButton';
+import { RunningScreenNavigationProp } from '@/scripts/navigation';
+import EmptyHeartIcon from '@/assets/icons/emptyHeart.svg';
+import getSize from '@/scripts/getSize';
+import Colors from '@/constants/Colors';
+import Styles from '@/constants/Styles';
 
 const RunningScreen = () => {
   const navigation = useNavigation<RunningScreenNavigationProp>();
@@ -20,15 +27,31 @@ const RunningScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={Styles.container}>
       {/* 상단 러닝 정보 */}
-      <View style={[styles.infoContainer, isPaused && styles.pausedInfoContainer]}>
-        <Text style={styles.headerText}>현재 러닝 정보</Text>
-        <Text style={styles.timeText}>10:03:23</Text>
-        <Text style={styles.distanceText}>00.00KM</Text>
-        <View style={styles.statsContainer}>
-          <Text style={styles.statText}>페이스 6'04"</Text>
-          <Text style={styles.statText}>심박수 111 ♥</Text>
+      <View style={[
+        styles.infoContainer,
+        isPaused && styles.pausedInfoContainer
+      ]}>
+        <View style={styles.textContainer}>
+          <Text style={styles.headerText}>현재 러닝 정보</Text>
+          <Text style={styles.timeText}>10:03:23</Text>
+          <Text style={styles.distanceText}>00.00KM</Text>
+
+          <View style={styles.statsContainer}>
+            <View style={styles.statContainer}>
+              <Text style={styles.statLabelText}>페이스</Text>
+              <Text style={styles.statText}> 6'04"</Text>
+            </View>
+
+            <View style={styles.statContainer}>
+              <Text style={styles.statLabelText}>심박수</Text>
+              <View style={styles.heartContainer}>
+                <Text style={styles.statLabelText}>111</Text>
+                <EmptyHeartIcon />
+              </View>
+            </View>
+          </View>
         </View>
       </View>
 
@@ -48,64 +71,66 @@ const RunningScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1E1E1E',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
   infoContainer: {
     width: '100%',
-    padding: 20,
+    height: getSize(236),
     backgroundColor: '#000',
+    paddingHorizontal: getSize(16),
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
-    alignItems: 'center',
   },
   pausedInfoContainer: {
     height: '50%', // 일시정지 시 화면 확장
   },
+  textContainer: {
+    marginTop: getSize(48),
+  },
   headerText: {
-    fontSize: 16,
+    fontSize: getSize(24),
+    fontFamily: 'Pretendard-Bold',
     color: '#FFFFFF',
-    marginBottom: 10,
   },
   timeText: {
-    fontSize: 24,
+    fontSize: getSize(20),
+    fontFamily: 'Pretendard-SemiBold',
     color: '#FFFFFF',
-    marginBottom: 10,
+    marginTop: getSize(14),
   },
   distanceText: {
-    fontSize: 48,
-    color: '#B0FF3D',
-    fontWeight: 'bold',
-    marginBottom: 10,
+    fontSize: getSize(50),
+    color: Colors.main,
+    fontFamily: 'Pretendard-Black',
   },
   statsContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    paddingHorizontal: 20,
+    marginTop: getSize(17),
+    gap: getSize(20),
   },
-  statText: {
-    fontSize: 18,
+  statContainer: {
+    flexDirection: 'row',
+    gap: getSize(14),
+    alignItems: 'flex-end',
+  },
+  heartContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: getSize(8),
+  },
+  statLabelText: {
+    fontSize: getSize(18),
+    fontFamily: 'Pretendard-Bold',
     color: '#FFFFFF',
   },
+  statText: {
+    fontSize: getSize(20),
+    fontFamily: 'Pretendard-Bold',
+    color: 'white',
+  },
   buttonContainer: {
-    marginBottom: 50,
+    position: 'absolute',
+    bottom: getSize(92),
     flexDirection: 'row',
     justifyContent: 'center',
-  },
-  pauseButton: {
-    backgroundColor: '#B0FF3D',
-    borderRadius: 50,
-    padding: 20,
-  },
-  controlButton: {
-    backgroundColor: '#B0FF3D',
-    borderRadius: 50,
-    padding: 20,
-    marginHorizontal: 10,
   },
 });
 
