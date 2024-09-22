4a0fe46c7bfed10e52c4f5f8fcc4709b59bccbc7
shin0112 <wnslcosltimo12@gmail.com>
2024-09-22 19:59:41 +0900

    feature : 메인 코스 피드 헤더 작성

--- components/header/IconHeader.tsx
+++ components/header/IconHeader.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+import LocationIcon from '@/assets/icons/location.svg';
+import BottomArrowIcon from '@/assets/icons/bottomArrow.svg';
 import {
   CombinedHeader,
   CombinedHeaderProps,
+  SearchIcon,
 } from '@/components/header/Header';
+import getSize from '@/scripts/getSize';
+import { TouchableOpacity } from 'react-native';
+import Fonts from '@/constants/Fonts';
 
 const { width } = Dimensions.get('window');
 
@@ -168,11 +182,72 @@ export const CourseMyHomeHeader: React.FC<CombinedHeaderProps> = ({
   );
 };
 
+type SearchStackList = {
+  "course-feed/search": undefined;
+  "course-feed/home": undefined;
+};
+
+type SearchNavigationProps = StackNavigationProp<
+  SearchStackList,
+  "course-feed/home"
+>;
+
+const SearchNavIcon = () => {
+  const navigation = useNavigation<SearchNavigationProps>();
+
+  return (
+    <SearchIcon onPress={() => { navigation.navigate('course-feed/search') }} />
+  );
+};
+
+export const CourseFeedMainHeader: React.FC = ({ }) => {
+  const pressArrowIcon = () => {
+    // to do : 위치 모달창 열기
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <LocationIcon width={getSize(16.36)} height={getSize(24)} />
+        <Text style={styles.leftText}>전체</Text>
+        <TouchableOpacity
+          style={styles.arrowIconContainer}
+          onPress={pressArrowIcon}
+        >
+          <BottomArrowIcon width={getSize(24)} height={getSize(13.85)} />
+        </TouchableOpacity>
+      </View>
+      <SearchNavIcon />
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     width: width,
+    height: getSize(29),
+    marginTop: getSize(58),
+    paddingHorizontal: getSize(16),
   },
+  leftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: getSize(29),
+  },
+  leftText: {
+    color: 'white',
+    fontSize: getSize(24),
+    fontFamily: Fonts.regular,
+    marginLeft: getSize(8),
+  },
+  arrowIconContainer: {
+    justifyContent: 'center',
+    height: getSize(29),
+    width: getSize(24),
+    marginLeft: getSize(12),
+  }
 });
 
 export {
