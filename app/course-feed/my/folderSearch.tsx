55031300562cdb37b1f79e808c8ec2e93b01b365
shin0112 <wnslcosltimo12@gmail.com>
2024-09-23 15:21:14 +0900

    feature : 내 폴더 찾기 화면 구성

---
+++ app/course-feed/my/folderSearch.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+
+import { MainGradient } from "@/components/Gradient";
+import Styles from "@/constants/Styles";
+
+const FolderSearchScreen = () => {
+  return (
+    <View style={Styles.container}>
+      <MainGradient />
+    </View>
+  );
+}
+
+export default FolderSearchScreen;
