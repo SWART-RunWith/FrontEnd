34c8bedba2c5f02614a0798e4aa88def8b1a88be
shin0112 <wnslcosltimo12@gmail.com>
2024-09-24 02:44:19 +0900

    feature : 코스 탐색 화면 추가

---
+++ app/course-feed/search.tsx
@@ -0,0 +1,13 @@
+import { MainGradient } from "@/components/Gradient";
+import Styles from "@/constants/Styles";
+import { View } from "react-native";
+
+const CourseFeedSearchScreen = () => {
+  return (
+    <View style={Styles.container}>
+      <MainGradient />
+    </View>
+  );
+};
+
+export default CourseFeedSearchScreen;
