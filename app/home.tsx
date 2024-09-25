af22ff12441d257a6dc8cddb456c52f8177478b6
shin0112 <wnslcosltimo12@gmail.com>
2024-09-26 04:05:19 +0900

    feature : 홈 화면에 지도 연결

--- app/home.tsx
+++ app/home.tsx
@@ -1,35 +1,100 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   View,
   Text,
-  TouchableOpacity,
   StyleSheet,
   Dimensions,
-  Button
+  Platform
 } from 'react-native';
+import MapView, {
+  MapMarker,
+  Polyline
+} from 'react-native-maps';
 import { useNavigation } from '@react-navigation/native';
+import * as Location from 'expo-location';
+import haversine from 'haversine';
 
-import Colors from '@/constants/Colors';
-import Sizes from '@/constants/Sizes';
+import LocationIcon from '@/assets/icons/location.svg';
 import Styles from '@/constants/Styles';
-import getSize from '@/scripts/getSize';
+import MapStyles from '@/constants/mapStyles.json';
+import { DefaultButton } from '@/components/button/Button';
 import { StartButton } from '@/components/button/RunningButton';
-import { DefaultButton, } from '@/components/button/Button';
+import { HomeHeader } from '@/components/header/TextOnlyHeader';
 import { HomeScreenNavigationProp } from '@/scripts/navigation';
+import getSize from '@/scripts/getSize';
 
-const { width, height } = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
-const HomeScreen: React.FC = ({ }) => {
+const LATITUDE_DELTA = 0.01;
+const LONGITUDE_DELTA = 0.01;
+
+const HomeScreen: React.FC = () => {
   const navigation = useNavigation<HomeScreenNavigationProp>();
+
+  const [coordinates, setCoordinates] = useState<{ latitude: number; longitude: number } | null>(null);
+  const mapRef = useRef<MapView>(null);
+  const markerRef = useRef<MapMarker>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Location permission not granted');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+
+      setCoordinates({ latitude, longitude });
+
+      Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.Balanced,
+          timeInterval: 300,
+          distanceInterval: 1,
+        },
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          const newCoordinate = { latitude, longitude };
+          setCoordinates(newCoordinate);
+        }
+      );
+    })();
+  }, []);
 
   const handleStartPress = () => {
     console.log('START button pressed')
-    navigation.replace('running/countDown');
+    navigation.replace('running/running');
   };
 
   return (
     <View style={Styles.container}>
-      <Text style={styles.title}>RUNWITH</Text>
+      {coordinates && (
+        <MapView
+          style={styles.map}
+          ref={mapRef}
+          customMapStyle={MapStyles}
+          initialRegion={{
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+          showsUserLocation={true}
+          followsUserLocation={true}
+        >
+          <MapMarker
+            ref={markerRef}
+            coordinate={coordinates}
+          >
+            <LocationIcon />
+          </MapMarker>
+        </MapView>
+      )}
+
+      <HomeHeader />
+
       <StartButton
         style={styles.button}
         onPress={() => handleStartPress()}
@@ -46,23 +111,20 @@ const HomeScreen: React.FC = ({ }) => {
 };
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: Sizes.headerTitle,
-    fontFamily: 'Hanson',
-    color: Colors.main,
+  map: {
     position: 'absolute',
-    top: getSize(62),
-    marginBottom: getSize(Sizes.formMargin), // 타이틀 아래 공간 확보
+    width: '100%',
+    height: '100%',
   },
   button: {
     position: 'absolute',
     bottom: getSize(140),
   },
   box: {
-    height: getSize(90),
-    width: width,
     backgroundColor: '#565656',
     position: 'absolute',
+    height: getSize(90),
+    width: width,
     bottom: 0,
   },
 });
