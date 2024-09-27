fbc52dbcd54b832cea1108caa81690af8cfac3cc
shin0112 <wnslcosltimo12@gmail.com>
2024-09-28 01:49:38 +0900

    feature : 크루 피드, 기록 빈 화면 구성

---
+++ app/user/record.tsx
@@ -0,0 +1,13 @@
+import { View } from "react-native"
+
+import Styles from "@/constants/Styles"
+
+const RecordScreen = () => {
+  return (
+    <View style={Styles.container}>
+
+    </View>
+  )
+}
+
+export default RecordScreen;
