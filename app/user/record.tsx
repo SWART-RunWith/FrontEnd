44d98612cd77b8e827139c7297b62f6c3382e6c2
shin0112 <wnslcosltimo12@gmail.com>
2024-09-29 07:16:19 +0900

    feature : record 화면 요소 구성 및 디자인 세팅

--- app/user/record.tsx
+++ app/user/record.tsx
@@ -1,13 +1,142 @@
-import { View } from "react-native"
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import Styles from "@/constants/Styles"
+import BackIcon from '@/assets/icons/back.svg';
+import CalendarIcon from '@/assets/icons/calendar_m.svg';
+import Styles from '@/constants/Styles';
+import Colors from '@/constants/Colors';
+import Fonts from '@/constants/Fonts';
+import BottomTab from '@/components/BottomTab';
+import { CustomCalendar } from '@/components/Calendar';
+import getSize from '@/scripts/getSize';
+
+const { width } = Dimensions.get('window');
 
 const RecordScreen = () => {
+  const navigation = useNavigation();
+
+  const [user, setUser] = useState('홍여준');
+  const [count, setCount] = useState(3);
+
   return (
     <View style={Styles.container}>
+      <View style={styles.firstR} />
+      <View style={styles.secondR} />
 
+      <View style={styles.topContainer}>
+        <View style={styles.header}>
+          <TouchableOpacity
+            style={[styles.iconContainer, { justifyContent: 'flex-start' }]}
+            onPress={() => navigation.goBack()}
+          >
+            <BackIcon width={getSize(13.85)} height={getSize(26)} />
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.iconContainer, { justifyContent: 'flex-end' }]}>
+            <CalendarIcon width={getSize(24)} height={getSize(26)} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ marginTop: getSize(21) }} />
+        <CustomCalendar />
+      </View>
+
+      <View style={styles.textBox}>
+        <View style={styles.textRow}>
+          <Text style={styles.text}>{user}님, 이번 달 총 </Text>
+          <Text style={[styles.text, { color: Colors.main }]}>{count}번</Text>
+          <Text style={styles.text}> 뛰셨어요.</Text>
+        </View>
+        <View style={styles.textRow}>
+          <Text style={styles.runwith}>RUNWITH</Text>
+          <Text style={styles.text}>과 더 힘차게 달려볼까요?</Text>
+        </View>
+      </View>
+
+      <BottomTab route='Record' reload={false} />
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: width,
+    height: getSize(29),
+    marginTop: getSize(58),
+    paddingHorizontal: getSize(16),
+  },
+  firstR: {
+    position: 'absolute',
+    backgroundColor: '#181818',
+    height: getSize(475),
+    width: width - getSize(58),
+    top: getSize(95),
+    left: getSize(29),
+    borderBottomEndRadius: 20,
+  },
+  secondR: {
+    position: 'absolute',
+    backgroundColor: '#181818',
+    height: getSize(488),
+    width: width - getSize(390 - 302),
+    top: getSize(95),
+    left: getSize(44),
+    borderBottomEndRadius: 20,
+  },
+  topContainer: {
+    backgroundColor: '#1B1B1B',
+    borderBottomEndRadius: 20,
+    shadowColor: 'rgba(0, 0, 0, 0.25)',
+    shadowOffset: {
+      width: 0,
+      height: 6
+    },
+    shadowRadius: 5.5,
+    elevation: 5.5,
+    shadowOpacity: 1,
+    width: width,
+    height: getSize(552),
+  },
+  iconContainer: {
+    width: getSize(24),
+  },
+  title: {
+    color: Colors.main,
+    fontSize: getSize(24),
+    fontWeight: 'bold',
+  },
+  textBox: {
+    backgroundColor: Colors.grayBox,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    height: getSize(110),
+    width: width - getSize(80),
+    marginTop: getSize(55),
+  },
+  textRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: getSize(16),
+    fontFamily: Fonts.medium,
+  },
+  runwith: {
+    color: Colors.main,
+    fontSize: getSize(16),
+    fontFamily: Fonts.hanson,
+  },
+});
 
 export default RecordScreen;
