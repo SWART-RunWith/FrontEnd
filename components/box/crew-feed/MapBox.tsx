291e8d0156ee83dd318066479d73e37014ef9457
shin0112 <wnslcosltimo12@gmail.com>
2024-10-03 03:36:15 +0900

    feature : 러닝 크루 박스 컴포넌트 작성

--- components/box/crew-feed/MapBox.tsx
+++ components/box/crew-feed/MapBox.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+
+import LocationIcon from '@/assets/icons/location.svg';
+import OptionIcon from '@/assets/icons/option.svg';
+import Colors from "@/constants/Colors";
+import Fonts from "@/constants/Fonts";
+import getSize from "@/scripts/getSize";
 
 interface CrewFeedMapBoxProp {
   crewId: number;
@@ -20,13 +26,90 @@ export const CrewFeedMapBox: React.FC<CrewFeedMapBoxProp> = ({
 }) => {
   return (
     <View style={styles.container}>
+      <View style={styles.locationContainer}>
+        <LocationIcon width={getSize(17)} height={getSize(24)} />
+        <Text style={styles.location}>{location}</Text>
+      </View>
 
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+      </View>
+
+      <View style={styles.contentContainer}>
+        <Text style={styles.content}>{content}</Text>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={onPressButton}>
+        <Text style={styles.buttonText}>크루 보기</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.optionIcon} onPress={onPressOption}>
+        <OptionIcon height={getSize(22)} width={getSize(8)} />
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: Colors.darkGrayBox,
+    borderRadius: 20,
+    width: getSize(358),
+    height: getSize(187),
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: getSize(20),
+    marginTop: getSize(17),
+    gap: getSize(6),
+  },
+  location: {
+    color: 'white',
+    fontSize: getSize(12),
+    fontFamily: Fonts.semiBold,
+    height: getSize(14),
+  },
+  nameContainer: {
+    marginLeft: getSize(20),
+  },
+  name: {
+    color: Colors.main,
+    fontSize: getSize(26),
+    fontFamily: Fonts.bold,
+  },
+  contentContainer: {
+    marginHorizontal: getSize(21),
+    marginTop: getSize(10),
+  },
+  content: {
+    color: 'white',
+    fontSize: getSize(12),
+    fontFamily: Fonts.light,
+  },
+  button: {
+    position: 'absolute',
+    backgroundColor: Colors.main,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    width: getSize(121),
+    height: getSize(36),
+    right: getSize(20),
+    bottom: getSize(17),
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: getSize(16),
+    fontFamily: Fonts.semiBold,
+    height: getSize(19),
+  },
+  optionIcon: {
+    position: 'absolute',
+    right: getSize(28),
+    top: getSize(23),
+    width: getSize(22),
+    height: getSize(22),
+    alignItems: 'flex-end',
   },
 })
